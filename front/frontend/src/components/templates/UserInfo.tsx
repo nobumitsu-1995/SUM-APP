5274f435ed4d088dad83e66a9c7e8f9b3d80fa81
masugata1995 <masugata1995@gmail.com>
2021-12-20 02:54:22 +0900

    read meを編集

--- front/frontend/src/components/templates/UserInfo.tsx
+++ front/frontend/src/components/templates/UserInfo.tsx
@@ -3,14 +3,13 @@ import { AccountBoxOutlined } from "@mui/icons-material";
 import { Typography } from "@mui/material";
 import { push } from "connected-react-router";
 import { useDispatch, useSelector } from "react-redux";
-import { getUserEmail, getUserIcon, getUserId, getUserName } from "../../reducks/users/selectors";
+import { getUserEmail, getUserIcon, getUserName } from "../../reducks/users/selectors";
 import Card from "../molecules/Card";
 
 const UserInfo = () => {
     const selector = useSelector(state => state);
     const username = getUserName(selector);
     const icon = getUserIcon(selector);
-    const id = getUserId(selector);
     const email = getUserEmail(selector);
     const dispatch = useDispatch();
 
@@ -31,9 +30,6 @@ const UserInfo = () => {
                     />
                     <Typography variant="h5" component="h2">
                         {username}
-                    </Typography>
-                    <Typography color="textSecondary">
-                        ID：{id}
                     </Typography>
                     <Typography color="textSecondary">
                         E-mail：{email}
